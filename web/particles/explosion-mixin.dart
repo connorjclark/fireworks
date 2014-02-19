90def9f02cf355340cf3fcb432ac9a3205a65ed4
Hoten <cjamcl@gmail.com>
2014-02-18 20:31:38 -0600

    decreased repetition in ExplosionMixin

--- web/particles/explosion-mixin.dart
+++ web/particles/explosion-mixin.dart
@@ -1,29 +1,34 @@
 part of particles;
 
 abstract class ExplosionMixin {
+  final Particle mold;
+  final int numParticles;
+  
+  ExplosionMixin({this.mold, this.numParticles});
+  
   List<Particle> explode(ParticlePool pool, num x, num y);
+  
+  //dist = integral of initialSpeed * drag^t * dt from 0 to 1
+  //     = (drag - 1) * initialSpeed / ln(drag)
+  //solve for necessary initial speed given following constraints:
+  // (1) particle's speed is multiplied by a drag value w/ respect to time
+  // (2) given the particle lives long enough, it will stop moving at
+  //     a distance of 'dist' from the center
+  num _calculateSpeed(num dist) => dist * log(mold.drag) / (mold.drag - 1);
 }
 
-class RingExplosion implements ExplosionMixin {
-  final int numParticles;
+class RingExplosion extends ExplosionMixin {
   final num radius;
-  final Particle mold;
   
-  RingExplosion({this.mold, this.numParticles, this.radius});
+  RingExplosion({Particle mold, int numParticles, this.radius}) :
+    super(mold: mold, numParticles: numParticles);
   
   List<Particle> explode(ParticlePool pool, num x, num y) {
-    //radius = integral of initialSpeed * drag^t * dt from 0 to 1
-    //       = (drag - 1) * initialSpeed / ln(drag)
-    //solve for necessary initial speed given following constraints:
-    // (1) particle's speed is multiplied by a drag value w/ respect to time
-    // (2) given the particle lives long enough, it will stop moving at
-    //     a distance of 'radius' from the center
-    final speed = radius * log(mold.drag) / (mold.drag - 1);
-    
+    final speed = _calculateSpeed(radius);
     final thetaOffset = range(0, PI2);
-    
     return new List.generate(numParticles, (i) {
       final theta = PI2 * i / numParticles + thetaOffset;
+      
       final particle = pool.createFrom(mold);
       particle
           ..x = x 
@@ -35,12 +40,11 @@ class RingExplosion implements ExplosionMixin {
   }
 }
 
-class ShapeExplosion implements ExplosionMixin {
-  final Particle mold;
-  final int numParticles;
+class ShapeExplosion extends ExplosionMixin {
   final Shape shape;
   
-  ShapeExplosion({this.mold, this.numParticles, this.shape});
+  ShapeExplosion({Particle mold, int numParticles, this.shape}) :
+    super(mold: mold, numParticles: numParticles);
   
   List<Particle> explode(ParticlePool pool, num x, num y) {
     var runningLength = 0;
@@ -61,12 +65,10 @@ class ShapeExplosion implements ExplosionMixin {
       final dir = shape.directions[currentIndex];
       final point = new Point(shape.vertices[currentIndex].x + dir.x * currentLength * interpolated, shape.vertices[currentIndex].y + dir.y * currentLength * interpolated);
       final distance = point.length;
-      
-      final speed = distance * log(mold.drag) / (mold.drag - 1);
-      
       final theta = atan2(point.y, point.x) + thetaOffset;
-      final particle = pool.createFrom(mold);
+      final speed = _calculateSpeed(distance);
       
+      final particle = pool.createFrom(mold);
       particle
           ..x = x 
           ..y = y
@@ -77,14 +79,14 @@ class ShapeExplosion implements ExplosionMixin {
   }
 }
 
-class ParametricExplosion implements ExplosionMixin {
+class ParametricExplosion extends ExplosionMixin {
   final xt, yt;
-  final Particle mold;
-  final int numParticles;
   
-  ParametricExplosion({num this.xt(num t), num this.yt(num t), this.mold, this.numParticles});
+  ParametricExplosion({num this.xt(num t), num this.yt(num t), Particle mold, int numParticles}) :
+    super(mold: mold, numParticles: numParticles);
   
-  ParametricExplosion.ellipse({num xRadius, num yRadius, this.mold, this.numParticles}) :
+  ParametricExplosion.ellipse({num xRadius, num yRadius, Particle mold, int numParticles}) :
+    super(mold: mold, numParticles: numParticles),
     xt = ((t) => xRadius * cos(t * PI2)),
     yt = ((t) => yRadius * sin(t* PI2));
   
@@ -97,12 +99,10 @@ class ParametricExplosion implements ExplosionMixin {
       final t = i / numParticles;
       final point = new Point(xt(t), yt(t));
       final distance = point.length;
-      
-      final speed = distance * log(mold.drag) / (mold.drag - 1);
-      
       final theta = atan2(point.y, point.x) + thetaOffset;
-      final particle = pool.createFrom(mold);
+      final speed = _calculateSpeed(distance);
       
+      final particle = pool.createFrom(mold);
       particle
           ..x = x 
           ..y = y
